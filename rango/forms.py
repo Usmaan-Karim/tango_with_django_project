b5f74e7e74e671c8dc41b01a83a52c115a20505a
Usmaan Karim <2475568K@student.gla.ac.uk>
2021-02-08 00:13:22 +0000

    Chapter 7, had to make some minor changes, clean() does nothing?

--- rango/forms.py
+++ rango/forms.py
@@ -13,9 +13,13 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    class Meta:
+        model = Page
+        exclude = ('category',)
+    
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -25,8 +29,4 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
         
         return cleaned_data
-    
-    class Meta:
-        model = Page
-        exclude = ('category',)
 
